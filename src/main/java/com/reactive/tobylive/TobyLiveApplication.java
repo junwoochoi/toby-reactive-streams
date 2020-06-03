3a30b2ee1d36fe7ad580eb388bcdf6d7464bdb83
junwoochoi <cjw4690@gmail.com>
2020-06-03 21:34:13 +0900

    Initial commit

---
+++ src/main/java/com/reactive/tobylive/TobyLiveApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.tobylive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobyLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobyLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reactive/tobylive/TobyLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.tobylive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
