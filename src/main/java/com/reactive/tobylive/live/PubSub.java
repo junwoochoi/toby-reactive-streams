ebc0c1e36cb195bc72d9fca18a5d71281b72af1c
junwoochoi <cjw4690@gmail.com>
2020-06-03 22:18:48 +0900

    refactor code

--- src/main/java/com/reactive/tobylive/live/PubSub.java
+++ src/main/java/com/reactive/tobylive/live/PubSub.java
@@ -5,41 +5,21 @@ import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 @Slf4j
 public class PubSub {
 
     public static void main(String[] args) {
-        Publisher<Integer> pub = new Publisher<Integer>() {
-            Iterable<Integer> iter = Stream.iterate(1, integer -> integer + 1)
-                    .limit(10)
-                    .collect(toList());
+        Publisher<Integer> pub = iterPub(Stream.iterate(1, integer -> integer + 1).limit(10).collect(Collectors.toUnmodifiableList()));
 
-            @Override
-            public void subscribe(Subscriber<? super Integer> sub) {
-                sub.onSubscribe(new Subscription() {
-                    @Override
-                    public void request(long n) {
-                        try {
-                            iter.forEach(sub::onNext);
-                            sub.onComplete();
-                        } catch (Throwable t) {
-                            sub.onError(t);
-                        }
-                    }
+        pub.subscribe(logSub());
+    }
 
-                    @Override
-                    public void cancel() {
-
-                    }
-                });
-            }
-        };
-
-        Subscriber<Integer> sub = new Subscriber<Integer>() {
+    private static Subscriber<Integer> logSub() {
+        return new Subscriber<Integer>() {
             @Override
             public void onSubscribe(Subscription subscription) {
                 log.debug("onSubscribe");
@@ -61,7 +41,29 @@ public class PubSub {
                 log.debug("onComplete");
             }
         };
+    }
 
-        pub.subscribe(sub);
+    private static Publisher<Integer> iterPub(List<Integer> iter) {
+        return new Publisher<Integer>() {
+            @Override
+            public void subscribe(Subscriber<? super Integer> sub) {
+                sub.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {
+                        try {
+                            iter.forEach(sub::onNext);
+                            sub.onComplete();
+                        } catch (Throwable t) {
+                            sub.onError(t);
+                        }
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
     }
 }
